761274dc597541c19b71ca1e628c3eaff14934bd
Moreshwar <vanmaremd@gmail.com>
2025-01-25 15:26:55 +0530

    initial comiit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/ui/app-sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,11 +20,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <head>{/* Add any meta tags or additional head content here */}</head>
+      <body>
+        <SidebarProvider>
+          <AppSidebar>
+            <SidebarTrigger />
+            {children}
+          </AppSidebar>
+        </SidebarProvider>
       </body>
     </html>
   );
